417043768d9d0a216f6565009ffaf889db67e558
김선민 <tjsals116@gmail.com>
2025-01-05 18:14:39 +0900

    ⚰️ remove storybook example code

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,23 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/index.ts',  // 라이브러리 엔트리 파일
+      name: 'DesunSystemReact',  // 라이브러리 이름
+      fileName: (format) => `desun-system-react.${format}.js`,  // 빌드된 파일 이름
+    },
+    rollupOptions: {
+      // 외부 종속성 설정 (예: React, ReactDOM 등)
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // '@/로 'src' 경로를 별칭으로 사용
